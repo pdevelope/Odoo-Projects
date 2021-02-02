2f74bbb8c9a7581ef5889fb6d2ae6492650ec142
pdevelope <49988605+pdevelope@users.noreply.github.com>
2021-02-02 17:45:12 +0100

    Add files via upload

---
+++ meetings_user/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+{
+ 'name': 'Meetings App Colaborativa',
+ 'version': '1.0',
+ 'author': 'CEO',
+ 'description': """ Modulo extension de Meetings App.""",
+ 'depends': ['meetings_app','mail'],
+ 'data': ['views/meetings_user_views.xml'],
+}
